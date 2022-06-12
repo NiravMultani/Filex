119e6a68a14185d0784af81f6e84f48149093d0d
Nirav Multani <nfmtheknight@gmail.com>
2022-06-12 19:20:56 +0530

    created structure for cloud services & file operation apis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AzureBlobModule } from './components/cloud-services/azure/azure-blob/azure-blob.module';
+import { AwsS3Module } from './components/cloud-services/aws/aws-s3/aws-s3.module';
+import { FileManagerModule } from './components/file-manager/file-manager.module';
 
 @Module({
-  imports: [],
+  imports: [AzureBlobModule, AwsS3Module, FileManagerModule],
   controllers: [AppController],
   providers: [AppService],
 })
