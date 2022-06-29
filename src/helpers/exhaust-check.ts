877fca474c2f98d35a205aeb167c65b4dd3d2c47
Nirav Multani <nfmtheknight@gmail.com>
2022-06-29 18:55:06 +0530

    fixed exhaustive check

--- src/helpers/exhaust-check.ts
+++ src/helpers/exhaust-check.ts
@@ -2,7 +2,7 @@ import { Logger } from '@nestjs/common';
 
 const logger = new Logger('exhaust-check');
 
-export const exhaustiveCheck = (value: string): never => {
+export const exhaustiveCheck = (value: never): never => {
   logger.debug('exhaustiveCheck : unhandled value = ', value);
   throw new Error('unhandled variable value');
 };
