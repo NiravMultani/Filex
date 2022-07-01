f9a9c6a5bfa21d3cddee2f6ced1744f9579555ed
Nirav Multani <nfmtheknight@gmail.com>
2022-07-01 16:30:54 +0530

    [WIP] env validation

--- src/config/config.validation.ts
+++ src/config/config.validation.ts
@@ -1,11 +1,24 @@
+import { CloudProviders } from './consts';
+
 export function validateEnv(config: Record<string, unknown>) {
   if (!config.PORT) {
-    throw new Error('PORT is required');
+    throw new Error('Env variable PORT is required');
   }
 
-  // TODO: check if ACTIVE_CLOUD_SERVICE is there
+  if (
+    !config.ACTIVE_CLOUD_SERVICE ||
+    ![CloudProviders.AWS, CloudProviders.AZURE].includes(
+      config.ACTIVE_CLOUD_SERVICE as CloudProviders,
+    )
+  ) {
+    throw new Error(
+      `Env variable ACTIVE_CLOUD_SERVICE is required and it can be 'aws' or 'azure'`,
+    );
+  }
 
   // TODO: based on cloud service validate required fields
+  if (config.ACTIVE_CLOUD_SERVICE === CloudProviders.AWS) {
+  }
 
   return config;
 }
