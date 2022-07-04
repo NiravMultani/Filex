ac24d810cbb1f89e84a156f10e3bd2b02227d5fa
Nirav Multani <nfmtheknight@gmail.com>
2022-07-04 15:16:39 +0530

    added helmet for security

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // TODO: use helmet
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
